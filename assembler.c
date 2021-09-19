c416add42d9510667fd99dbd2846b468573a1b4e
Masaad Khan <masaadkhan123@gmail.com>
2021-09-19 06:25:10 -0500

    Productive work, working add instruction it looks like

--- assembler.c
+++ assembler.c
@@ -28,7 +28,6 @@ readAndParse(FILE* pInfile, char* pLine, char** pLabel,
     pLine[i] = tolower(pLine[i]);
   }
 
-  printf("String: %s\n", pLine);
   *pLabel = *pOpcode = *pArg1 = *pArg2 = *pArg3 = *pArg4 = pLine + strlen(pLine);
 
   /* ignore the comments */
@@ -44,14 +43,19 @@ readAndParse(FILE* pInfile, char* pLine, char** pLabel,
   }
 
   /* found a label */
-  if(isOpcode(lPtr) == -1 && lPtr[0] != '.') {
+  if(!isOpcode(lPtr) && lPtr[0] != '.') {
     *pLabel = lPtr;
     if(!(lPtr = strtok( NULL, "\t\n ,"))) {
+      // checks for end of line
       return(OK);
     }
   }
     
   *pOpcode = lPtr;
+  if (string_equal(*pOpcode, ".end")) {
+    printf("We found .end!\n");
+  }
+  printf("%s\n", *pOpcode);
 
   if(!(lPtr = strtok(NULL, "\t\n ,"))) {
     return(OK);
@@ -152,15 +156,323 @@ toNum(char* pStr) {
   }
 }
 
-int
+bool
 isOpcode(char* str) {
-  printf("TODO: isOpcode\n");
-  if (str == "Add") {
+  //todo: isOpcode
+  if (string_equal(str, "add")) {
+    return true;
+  } else if (string_equal(str, "and")) {
+    return true;
+  }
+  else if(string_equal(str, "br") ||
+          string_equal(str, "brp") || 
+          string_equal(str, "brz") ||
+          string_equal(str, "brnp") ||
+          string_equal(str, "brnz") ||
+          string_equal(str, "brnzp") ||
+          string_equal(str, "brzp")) {
+    return true;
+  }
+  else if(string_equal(str, "jmp") ||
+          string_equal(str, "ret")){
+    return true;
+  }
+  else if(string_equal(str, "jsr") ||
+          string_equal(str, "jsrr")){
+    return true;
+  }
+  else if(string_equal(str, "ldb")){
+    return true; 
+  }
+  else if(string_equal(str, "ldw")){
+    return true;
+  }
+  else if(string_equal(str, "lea")){
+    return true; 
+  }
+  else if(string_equal(str, "rti")){
+    return true;
+  }
+  else if(string_equal(str, "lshf") ||
+          string_equal(str, "rshfl") ||
+          string_equal(str, "rshfa")){
+    return true;
+  }
+  else if(string_equal(str, "stb")){
+    return true;
+  }
+  else if(string_equal(str, "stw")){
+    return true;
+  }
+  else if(string_equal(str, "trap")){
+    return true;
+  }
+  else if(string_equal(str, "xor") ||
+          string_equal(str, "not")){
+    return true;
+  }
 
-  } else if (str == "And") {
+  /* String is not an Opcode */
+  return false;
+}
+
+struct label g_labels[MAX_NUM_LABELS];
+
+void
+label_exists(char* label) {
+  for (int i = 0; i < MAX_NUM_LABELS; i++) {
+    if (g_labels[i].label != NULL) {
+      if (string_equal(g_labels[i].label, label)) {
+        printf("Found a label that is already init-ed\n");
+        exit(4);
+      }
+    }
+  }
+}
+
+void
+add_label(char* new_label, uint16_t* PC) {
+  /* If we find a pre-existing label, then label_exists will exit(4) */
+  label_exists(new_label);
+
+  for (int i = 0; i < MAX_NUM_LABELS; i++) {
+    if (g_labels[i].label != NULL) {
+      g_labels->addr = *PC;
+      strcpy(g_labels->label, new_label);
+      break;
+    }
+  }
+}
+
+void
+valid_register() {
+
+}
+
+void
+convert_args(char* arg) {
+  /* We can only get passed in registers or immediate values */
+  /* But you can get passed in labels for LEA, JSR, JSRR, and BR */
+  // if (arg[0] == 'r') {
+  //   // This argument must be a register
+  //   "r10"
+  //   int sum = 0;
+  //   for (int i = 1; i < strlen("r10"); i++) {
+  //     sum += arg[i];
+  //   }
+  //   sum -= arg[0]];
+  //   convert_str_to_num("10");
+  // }
+}
+
+int
+check_register(char* reg) {
+  if (reg[0] == 'r') {
+    char* reg_copy;
+
+    reg_copy = malloc(strlen(reg)*sizeof(char));
+    strcpy(reg_copy, reg);
+    reg_copy[0] = '#';
+
+    int reg_value = toNum(reg_copy);
+
+    free(reg_copy);
+
+    if (reg_value > 7) {
+      return -1;
+    } else {
+      return reg_value;
+    }
+  }
+
+  return -1;
+}
+
+void
+run(FILE* outfile, char* Label, char* Opcode, char* arg1, char* arg2, char* arg3, char* arg4) {
+  uint16_t instruction = 0;
+
+  // todo: check for which immediates are signed, which ones are not signed...
+  // todo: check if there is a # or x on the immediate arguments we get passed...
+  // todo: check for each instruction arg1, 2, 3, 4... 
+  // todo: check whether the negative values stuff is working as intended
+  // todo: check if all registers accessed are proper, etc...
+  // todo: check if the pcoffset, etc that the user inputs is valid
+  // todo: check the specifics of boffset6 
+  // todo: convert arg1.... to nums first.... but we also need to check labels
+  // todo: check if we need arg4... Is checking if an argument is NULL a good check for bad input... exit?
+  if (string_equal(Opcode, "add")) {
+    if (arg4 != NULL) {
+      // todo: maybe do an exit here...
+      printf("arg4 is not NULL\n");
+      exit(1);
+    }
+
+    int reg_value1 = check_register(arg1);
+    int reg_value2 = check_register(arg2);
+    if (!((reg_value1 < 0) || (reg_value2 < 0))) {
+      /* This third argument tells us whether it could be an immediate or a register */
+      /* todo: Should reg_value3 be able to be negative... */
+      int reg_value3 = check_register(arg3);
+      printf("reg_3: %d\n", reg_value3);
+      if (reg_value3 != -1) {
+        instruction = SET_OPCODE(ADD) | SET_DR(reg_value1) | SET_SR1(reg_value2) | SET_IMM_OR_REG_B5(REGISTER) | SET_SR2(reg_value3);
+      } else {
+        printf("We made it heere!! Bit_5: %d\n", SET_IMM_OR_REG_B5(1));
+        instruction = SET_OPCODE(ADD) | SET_DR(reg_value1) | SET_SR1(reg_value2) | SET_IMM_OR_REG_B5(IMMEDIATE) | SET_IMM5(toNum(arg3));
+        printf("tonum: %d\n", toNum(arg3));
+      }
+    }
+    printf("0x%x\n", instruction);
+    fprintf(outfile, "0x%x\n", instruction);
 
   }
-  return false;
+
+  else if (string_equal(Opcode, "and")) {
+    if(check_register(arg3)){
+      instruction = SET_OPCODE(AND) | SET_DR(toNum(arg1)) | SET_SR1(arg2) | SET_IMM_OR_REG_B5(0) | SET_SR2(arg3);
+    } else{
+      instruction = SET_OPCODE(AND) | SET_DR(toNum(arg1)) | SET_SR1(arg2) | SET_IMM_OR_REG_B5(1) | SET_IMM5(arg3);
+    }
+  }
+
+  else if(string_equal(Opcode, "br")) {
+    instruction = SET_OPCODE(BR) | SET_CC_N(0) | SET_CC_Z(0) | SET_CC_P(0);
+  }
+
+  else if(string_equal(Opcode, "brp")) {
+    instruction = SET_OPCODE(BR) | SET_CC_N(0) | SET_CC_Z(0) | SET_CC_P(1);
+  }
+
+  else if(string_equal(Opcode, "brz")) {
+    instruction = SET_OPCODE(BR) | SET_CC_N(0) | SET_CC_Z(1) | SET_CC_P(0);
+  }
+
+  else if(string_equal(Opcode, "brzp")) {
+    instruction = SET_OPCODE(BR) | SET_CC_N(0) | SET_CC_Z(1) | SET_CC_P(1);
+  }
+
+  else if(string_equal(Opcode, "brn")) {
+    instruction = SET_OPCODE(BR) | SET_CC_N(1) | SET_CC_Z(0) | SET_CC_P(0);
+  }
+
+  else if(string_equal(Opcode, "brnp")) {
+    instruction = SET_OPCODE(BR) | SET_CC_N(1) | SET_CC_Z(0) | SET_CC_P(1);
+  }
+
+  else if(string_equal(Opcode, "brnz")) {
+    instruction = SET_OPCODE(BR) | SET_CC_N(1) | SET_CC_Z(1) | SET_CC_P(0);
+  }
+
+  else if(string_equal(Opcode, "brnzp")) {
+    instruction = SET_OPCODE(BR) | SET_CC_N(1) | SET_CC_Z(1) | SET_CC_P(1);
+  }
+
+  else if(string_equal(Opcode, "jmp")) {
+    instruction = SET_OPCODE(JMP) | SET_BASE_R(toNum(arg1));
+  }
+
+  else if(string_equal(Opcode, "ret")) {
+    instruction = SET_OPCODE(RET) | SET_BASE_R(0x7);
+  }
+
+  else if(string_equal(Opcode, "jsr")) {
+    instruction = SET_OPCODE(JSR) | SET_IMM_OR_REG_B11(1) | SET_PCOFFSET11(toNum(arg1));
+  }
+
+  else if(string_equal(Opcode, "jsrr")) {
+    instruction = SET_OPCODE(JSRR) | SET_IMM_OR_REG_B11(0) | SET_BASE_R(toNum(arg1));
+  }
+
+  else if(string_equal(Opcode, "ldb")){
+    instruction = SET_OPCODE(LDB) | SET_DR(toNum(arg1)) | SET_BASE_R(arg2) | SET_BOFFSET6(arg3);
+  }
+
+  else if(string_equal(Opcode, "ldw")){
+    instruction = SET_OPCODE(LDW) | SET_DR(toNum(arg1)) | SET_BASE_R(arg2) | SET_OFFSET6(arg3);
+  }
+
+  else if(string_equal(Opcode, "lea")){
+    instruction = SET_OPCODE(LEA) | SET_DR(toNum(arg1)) | SET_PCOFFSET9(arg2);
+  }
+
+  else if(string_equal(Opcode, "rti")){
+    instruction = SET_OPCODE(RTI);
+  }
+
+  else if(string_equal(Opcode, "lshf") {
+    instruction = SET_OPCODE(LSHF) | SET_DR(toNum(arg1)) | SET_SR(arg2) | SET_SHF_TYPE(0) | SET_AMOUNT4(arg3);
+  }
+
+  else if(string_equal(Opcode, "rshfl") {
+    instruction = SET_OPCODE(RSHFL) | SET_DR(toNum(arg1)) | SET_SR(arg2) | SET_SHF_TYPE(1) | SET_AMOUNT4(arg3);
+  }
+
+  else if(string_equal(Opcode, "rshfa") {
+    instruction = SET_OPCODE(RSHFA) | SET_DR(toNum(arg1)) | SET_SR(arg2) | SET_SHF_TYPE(3) | SET_AMOUNT4(arg3);
+  }
+
+  else if(string_equal(Opcode, "stb")){
+    instruction = SET_OPCODE(STB) | SET_ST_SR(toNum(arg1)) | SET_BASE_R(arg2) | SET_BOFFSET6(arg3);
+  }
+
+  else if(string_equal(Opcode, "stw")){
+    instruction = SET_OPCODE(STW) | SET_ST_SR(toNum(arg1)) | SET_BASE_R(arg2) | SET_OFFSET6(arg3);
+  }
+
+  else if(string_equal(Opcode, "trap")){
+    instruction = SET_OPCODE(TRAP) | SET_TRAPVECT8(toNum(arg1));
+  }
+
+  else if(string_equal(Opcode, "xor")) {
+    if(check_register(arg3)){
+      instruction = SET_OPCODE(XOR) | SET_DR(toNum(arg1)) | SET_SR1(arg2) | SET_IMM_OR_REG_B5(0) | SET_SR2(arg3);
+    } else{
+      instruction = SET_OPCODE(XOR) | SET_DR(toNum(arg1)) | SET_SR1(arg2) | SET_IMM_OR_REG_B5(1) | SET_IMM5(arg3);
+    }
+  }
+
+  else if(string_equal(Opcode, "not")) {
+    instruction = SET_OPCODE(NOT) | SET_DR(toNum(arg1)) | SET_SR1(arg2) | SET_IMM_OR_REG_B5(1) | SET_SR2(0x1f);
+  }
+
+  else {
+    printf("Invalid Operand!\n");
+    // todo: exit here?!
+  }
+}
+
+void
+assembler_init(FILE** infile, FILE** outfile, char* prgName, char* iFileName, char* oFileName,
+               char** pLine, char*** pLabel, ) {
+  if (prgName == NULL ||
+      iFileName == NULL ||
+      oFileName == NULL) {
+    printf("Missing command line argument(s)!\n");
+    exit(4);
+  }
+
+  #if (DEBUG)
+  printf("program name = '%s'\n", prgName);
+  printf("input file name = '%s'\n", iFileName);
+  printf("output file name = '%s'\n", oFileName);
+  #endif
+
+  /* open the source file */
+  *infile = fopen(iFileName, "r");
+  *outfile = fopen(oFileName, "w");
+    
+  if (!(*infile)) {
+    printf("Error: Cannot open file %s\n", iFileName);
+    exit(4);
+  }
+
+  if (!(*outfile)) {
+    printf("Error: Cannot open file %s\n", oFileName);
+    exit(4);
+  }
+
+  /* Don't need to initialize the label global array everything set to 0 */
 }
 
 int
@@ -169,34 +481,8 @@ main(int argc, char* argv[]) {
   char *iFileName = argv[1];
   char *oFileName = argv[2];
 
-  if (prgName == NULL ||
-      iFileName == NULL ||
-      oFileName == NULL) {
-    printf("Missing command line argument(s)!\n");
-    exit(4);
-  }
-
-  printf("program name = '%s'\n", prgName);
-  printf("input file name = '%s'\n", iFileName);
-  printf("output file name = '%s'\n", oFileName);
-
-  /* open the source file */
-  FILE* infile = fopen(argv[1], "r");
-  FILE* outfile = fopen(argv[2], "w");
-    
-  if (!infile) {
-    printf("Error: Cannot open file %s\n", argv[1]);
-    exit(4);
-  }
-  if (!outfile) {
-    printf("Error: Cannot open file %s\n", argv[2]);
-    exit(4);
-  }
-
-  /* Do stuff with files */
-  fprintf(outfile, "Hello World!\n");
-  // maybe we change tonum function
-  printf("-1 toNum: %d\n", toNum("x-1"));
+  FILE* infile;
+  FILE* outfile;
 
   char pLine[MAX_LINE_LENGTH + 1];
   char* pLabel[MAX_LINE_LENGTH + 1];
@@ -206,13 +492,47 @@ main(int argc, char* argv[]) {
   char* pArg3[MAX_LINE_LENGTH + 1];
   char* pArg4[MAX_LINE_LENGTH + 1];
 
+  assembler_init(&infile, &outfile, prgName, iFileName, oFileName, &pLine, &pLabel, &pOpcode, &pArg1, &pArg2, &pArg3, &pArg4);
+
+  //maybe we change tonum function
+  // printf("-1 toNum: %d\n", toNum("x-1"));
+
   int lRet;
+
+  /* Do the First Pass and check for .ORIG/.END and Labels */
+  bool orig = false;
+  bool end = false;
+  do {
+    lRet = readAndParse(infile, pLine, pLabel, pOpcode, pArg1, pArg2, pArg3, pArg4);
+
+    if (lRet != DONE && lRet != EMPTY_LINE) {
+      // do stuff and write to out file
+      if (string_equal(*pOpcode, ".orig")) {
+        orig = true;
+      }
+      if (string_equal(*pOpcode, ".end")) {
+        printf("We got into this if!\n");
+        end = true;
+      }
+      // printf("%s\n", *pOpcode);
+    }
+  } while(lRet != DONE);
+
+  if (!orig || !end) {
+    #if (DEBUG)
+    printf("Was not able to find a .ORIG OR .END\n");
+    #endif
+    // exit(0); // todo: figure out why it can't detect .end...
+  }
+
+  rewind(infile);
 
   do {
     lRet = readAndParse(infile, pLine, pLabel, pOpcode, pArg1, pArg2, pArg3, pArg4);
 
     if (lRet != DONE && lRet != EMPTY_LINE) {
       // do stuff and write to out file
+      run(outfile, *pLabel, *pOpcode, *pArg1, *pArg2, *pArg3, *pArg4);
     }
   } while(lRet != DONE);
 
--- assembler.h
+++ assembler.h
@@ -1,8 +1,13 @@
+#include <string.h>
+#include <stdint.h>
+
 /***************************************************************************************/
 /*                                                                                     */
 /*                               DEFINES/MACROS/ENUMS                                  */
 /*                                                                                     */
 /***************************************************************************************/
+
+#define DEBUG 1
 
 enum
 {
@@ -11,14 +16,102 @@ enum
 
 /* Note: MAX_LINE_LENGTH, OK, EMPTY_LINE, and DONE are defined values */
 #define MAX_LINE_LENGTH 255
+#define MAX_NUM_LABELS 255
+#define MAX_LABEL_LEN 21
+
+#define address uint16_t
+
+// Labels
+struct label {
+	address addr;
+	char label[MAX_LABEL_LEN];
+};
 
 // Opcodes
-#define ADD 0x1
-#define AND 0xA
-// #define 
+enum opcode {
+  ADD = 0x1,
+  AND = 0xA,
 
-// Functions
-#define GET_DR(x) (((uint16_t)x & 0x0e00) >> 9)
+  BR = 0x0,
+  BRP = 0x0,
+  BRZ = 0x0,
+  BRZP = 0x0,
+  BRN = 0x0,
+  BRNP = 0x0,
+  BRNZ = 0x0,
+  BRNZP = 0x0,
+
+  JMP = 0xC,
+  RET = 0xC,
+
+  JSR = 0x4,
+  JSRR = 0x4,
+
+  LDB = 0x2,
+  LDW = 0x6,
+  LEA = 0xE,
+  RTI = 0x1,
+
+  LSHF = 0xD,
+  RSHFL = 0xD,
+  RSHFA = 0xD,
+
+  STB = 0x3,
+  STW = 0x7,
+  TRAP = 0xF,
+
+  XOR = 0x9,
+  NOT = 0x9,
+
+  INVALID = 0xFF,
+};
+
+#define string_equal(str1, str2) ((bool)(strcmp(str1, str2) == 0))
+
+// Getters
+#define GET_DR(x) ((((uint16_t)x) & 0x0e00) >> 9)
+#define GET_SR1(x) ((((uint16_t)x) & 0x01c0) >> 6)
+#define GET_BASE_R(x) ((((uint16_t)x) & 0x01c0) >> 6)
+
+#define GET_IMM_OR_REG(x) ((((uint16_t)x) & 0x0020) >> 5)
+
+#define GET_CC_N(x) ((((uint16_t)x) & 0x0800) >> 11)
+#define GET_CC_Z(x) ((((uint16_t)x) & 0x0400) >> 10)
+#define GET_CC_P(x) ((((uint16_t)x) & 0x0200) >> 9)
+
+#define GET_PCoffset9(x) (((uint16_t)x) & 0x01ff)
+
+//Setters
+#define SET_OPCODE(op) ((((uint16_t)op) & 0xf) << 12)
+
+#define SET_DR(dr) ((((uint16_t)dr) & 0x7) << 9)
+#define SET_SR(sr) ((((uint16_t)sr1) & 0x7) << 6)
+#define SET_ST_SR(st_sr) ((((uint16_t)st_sr) & 0x7) << 9)
+#define SET_SR1(sr1) ((((uint16_t)sr1) & 0x7) << 6)
+#define SET_SR2(sr2) ((((uint16_t)sr2) & 0x7) << 0)
+#define SET_BASE_R(base_r) ((((uint16_t)base_r) & 0x7) << 6)
+
+#define SET_IMM_OR_REG_B5(x) ((((uint16_t)x) & 0x1) << 5)
+#define SET_IMM_OR_REG_B11(x) ((((uint16_t)x) & 0x1) << 11)
+#define SET_SHF_TYPE(x) ((((uint16_t)x) & 0x2) << 4)
+
+#define SET_IMM5(imm5) ((((uint16_t)imm5) & 0x1f) << 0)
+#define SET_AMOUNT4(amount4) ((((uint16_t)amount4) & 0xf) << 0)
+
+#define SET_PCOFFSET9(pcoffset9) ((((uint16_t)pcoffset9) & 0x1ff) << 0)
+#define SET_PCOFFSET11(pcoffset11) ((((uint16_t)pcoffset11) & 0x7ff) << 0)
+#define SET_BOFFSET6(boffset6) ((((uint16_t)boffset6) & 0x2f) << 0)
+#define SET_OFFSET6(offset6) ((((uint16_t)offset6) & 0x2f) << 0)
+
+#define SET_CC_N(cc_n) ((((uint16_t)cc_n) & 0x1) << 11)
+#define SET_CC_Z(cc_z) ((((uint16_t)cc_z) & 0x1) << 10)
+#define SET_CC_P(cc_p) ((((uint16_t)cc_p) & 0x1) << 9)
+
+#define SET_TRAPVECT8(trapvect8) ((((uint16_t)trapvect8) & 0xff) << 0)
+
+// Magic Numbers
+#define REGISTER 0
+#define IMMEDIATE 1
 
 /***************************************************************************************/
 /*                                                                                     */
@@ -34,5 +127,5 @@ readAndParse(FILE* pInfile, char* pLine, char** pLabel,
              char** pOpcode, char** pArg1, char** pArg2,
              char** pArg3, char** pArg4);
 
-int
+bool
 isOpcode(char* str);
