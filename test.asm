5588f3dd467135588b5a2aeeb25fe263d2ef8699
Masaad Khan <masaadkhan123@gmail.com>
2021-09-20 16:50:07 -0500

    Made one new script, discuss these changes

--- test.asm
+++ test.asm
@@ -1,11 +1,3 @@
-;This program counts from 10 to 0
-	.ORIG x3000   	
-	LEA R0, TEN		;This instruction will be loaded into memory location x3000
-	LDW R1, R0, #0
-START	ADD R1, R1, #-1
-	BRZ DONE
-	BR START
-				;blank line
-DONE	TRAP x25		;The last executable instruction
-TEN	.FILL x000A		;This is 10 in 2's comp, hexadecimal
-	.END			;The pseudo-op, delimiting the source program
+.ORIG x1000
+	ADD R1, R0, 1	; 1 is an invalid operand (neither a register nor an immediate)
+	.END
